e17964f0b8593c06cc0e63f30dc1125ef19df927
codegarg <ridhamgarg002@gmail.com>
2025-07-15 15:23:49 +0530

    lec6

---
+++ LEC-6/task/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+fs.readFile('../demo.txt',"utf-8",function(err,data1) {
+    if(err) return console.log(err);
+    console.log(data1);
+    
+    fs.readFile('../demo2.txt',"utf-8",function(err,data2) {
+        if(err) return console.log(err);
+        console.log(data2);
+        
+        const data = data1 + "\n" + data2;
+        
+        fs.writeFile('../task.txt',data,function(err) {
+            if(err) return console.log(err);
+            console.log("success!!!!");
+        })
+    })
+})
