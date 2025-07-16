9c04a6af863bf5ebcf240dccd2dbe4be1c07ac1b
codegarg <ridhamgarg002@gmail.com>
2025-07-16 14:04:02 +0530

    assignment

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -6,7 +6,7 @@
 const fs = require("fs");
 const input = process.argv.slice(2).join(" ");
 
-fs.writeFile("../demo-assign.txt", input, function(err) {
+fs.writeFile("../assignment-1/demo-assign.txt", input, function(err) {
     if(err) return console.log(err);
     console.log("File written successfully!");
 });
