aa467fd673e155d77a296e9c49a38db5e2463c62
codegarg <ridhamgarg002@gmail.com>
2025-07-16 15:43:41 +0530

    LEC-7

--- LEC-7/read/index.js
+++ LEC-7/read/index.js
@@ -1,14 +1,21 @@
- const fs = require("fs");
-
+const fs = require("fs");
+const {read} = require("../IOoperation/util"); 
 
 // fs.readFile("../users.txt","utf-8",function(err,data){
 //     if(err) return console.log(err);
 //     console.log("Data read from file:", data);
 // })
 
-fs.readFile("../users.txt","utf-8",function(err,data){
-    if(err) return console.log(err);
-    // console.log("Data read from file:", data[0]);
-    let users = JSON.parse(data);
-    console.log(users[0].name);
-})
+// fs.readFile("../users.txt","utf-8",function(err,data){
+//     if(err) return console.log(err);
+//     // console.log("Data read from file:", data[0]);
+//     let users = JSON.parse(data);
+//     console.log(users[0].name);
+// })
+
+async function readfile(filepath){
+    let data = await read(filepath);
+    console.log(data);
+}
+
+readfile("../users.txt")
