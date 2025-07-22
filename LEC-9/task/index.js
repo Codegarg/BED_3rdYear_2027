8cfeef162a24a508215b4ed0c34663fab0d68983
codegarg <ridhamgarg002@gmail.com>
2025-07-22 16:53:08 +0530

    Lec-9

--- LEC-9/task/index.js
+++ LEC-9/task/index.js
@@ -1,23 +1,39 @@
+const { error } = require('console');
 const express = require('express');
 const app = express();
 const fs = require('fs');
 app.use(express.json());
 
 app.post('/user', (req, res) => {
-    res.send("User endpoint reached!");
-    console.log(req.body);
+    const newUser = req.body;
+    console.log(newUser);
 
-    let data = req.body;
-
-    fs.appendFile('users.txt', JSON.stringify(data,null,2), (err) => {
-        if (err) {
-            console.error('Error appending to file', err);
-        } else {
-            console.log('Data appended to file successfully');
+    fs.readFile('users.txt', 'utf-8', (err, fileData) => {
+        let allusers = [];
+        if (err && err.code !== 'ENOENT') {
+            // If a real error occurs (not just file not found), send an error response.
+            return res.status(500).json({ error: 'Failed to read user data.' });
         }
+
+        if (fileData) {
+            try {
+                allusers = JSON.parse(fileData);
+            } catch (e) {
+                return res.status(500).json({ error: 'Failed to parse user data.' });
+            }
+        }
+
+        allusers.push(newUser);
+
+        fs.writeFile('users.txt', JSON.stringify(allusers, null, 2), (writeErr) => {
+            if (writeErr) {
+                return res.status(500).send('Error writing to file');
+            }
+            res.send('Data written to file successfully');
+        });
     });
 });
 
-app.listen(4400),function(){
+app.listen(4400, function() {
     console.log('Server is running on port 4400');
-}
+});
