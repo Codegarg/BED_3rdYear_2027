2dbb603b80e27170a5772d0221d21f59137fed75
codegarg <ridhamgarg002@gmail.com>
2025-07-23 15:41:18 +0530

    LEC-10

--- LEC-10/index.js
+++ LEC-10/index.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.static(__dirname+"/public"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+const fs = require('fs');
 
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/index.html');
@@ -19,6 +20,33 @@ app.post('/users', (req, res) => {
         username,
         password
     })
+    const newUser = req.body;
+    console.log(newUser);
+    
+    fs.readFile('users.txt', 'utf-8', (err, fileData) => {
+        let allusers = [];
+        if (err && err.code !== 'ENOENT') {
+            // If a real error occurs (not just file not found), send an error response.
+             return res.status(500).json({ error: 'Failed to read user data.' });
+        }
+    
+        if (fileData) {
+             try {
+                 allusers = JSON.parse(fileData);
+            } catch (e) {
+                return res.status(500).json({ error: 'Failed to parse user data.' });
+            }
+        }
+    
+        allusers.push(newUser);
+    
+        fs.writeFile('users.txt', JSON.stringify(allusers, null, 2), (writeErr) => {
+            if (writeErr) {
+                return res.status(500).send('Error writing to file');
+            }
+            res.send('Data written to file successfully');
+        });
+    });
 });
 
 app.listen(4444 , ()=> {
