8ba63dbd6ae4be73a7d1440c90da65667c12c3ad
codegarg <ridhamgarg002@gmail.com>
2025-07-23 15:03:34 +0530

    LEC-10

--- LEC-10/index.js
+++ LEC-10/index.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname+"/public"));
 // app.get('/about',(req, res) => {
 //     res.sendFile(__dirname + '/about.html');
 // })
+app.post('/users', (req, res) => {
+    
+});
 
 app.listen(4444 , ()=> {
     console.log('Server is running on port 4444');
