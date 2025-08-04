9cf046f314479b1ac946eb7f19e894aece3f1a37
codegarg <ridhamgarg002@gmail.com>
2025-08-04 14:24:59 +0530

    LEC-13

--- LEC-13/index.js
+++ LEC-13/index.js
@@ -2,6 +2,10 @@ function getUsersData(URL){
     fetch(URL)
     .then((res)=>{
         console.log(res)
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data);
     })
     .catch((err)=>{
         console.error(err);
