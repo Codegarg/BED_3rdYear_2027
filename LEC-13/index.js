630ec27c8dfa62e2f438cf832dca788f110333c1
codegarg <ridhamgarg002@gmail.com>
2025-08-04 14:54:31 +0530

    LEC-13

--- LEC-13/index.js
+++ LEC-13/index.js
@@ -1,4 +1,6 @@
-function getUsersData(URL){
+let userContainer = document.querySelector('.user-container');
+console.log(userContainer);
+function getUsersData(URl){
     fetch(URL)
     .then((res)=>{
         console.log(res)
@@ -10,5 +12,19 @@ function getUsersData(URL){
     .catch((err)=>{
         console.error(err);
     });
+}
+
+function displayUsers(user){
+    let li = document.createElement('li');
+    li.setAttribute('class', 'user-item');
+    li.innerHTML = '<div class = "user-info">
+                <h1>${user.name}</h1>
+                <p>${user.email}</p>
+            </div>
+            <div class = "user-btn">
+                <button class = "user-delete">❌</button>
+                <button class = "user-edit">✏️</button>
+            </div>'
+    userContainer.appendChild(li);
 }
 getUsersData('https://jsonplaceholder.typicode.com/users');
