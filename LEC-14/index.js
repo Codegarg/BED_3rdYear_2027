3c51ee3f502f184ca6d3eb6facb975927dbe9745
codegarg <ridhamgarg002@gmail.com>
2025-08-05 15:08:43 +0530

    LEC-14

--- LEC-14/index.js
+++ LEC-14/index.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname+ '/public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/users', (req, res) => {
-    fs.readFile('users.json', 'utf8', function(err, data){
+    fs.readFile('users.json', 'utf-8', function(err, data){
         if (err) res.send(err);
         let allusers = JSON.parse(data);
         res.json(allusers);
