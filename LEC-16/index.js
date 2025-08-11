e00518b73174ead42203412ba2e80d5ff3f22d35
codegarg <ridhamgarg002@gmail.com>
2025-08-11 15:10:30 +0530

    LEC-16

--- LEC-16/index.js
+++ LEC-16/index.js
@@ -4,11 +4,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/blogs",(req,res)=>{
+const Blogs = require("./model/user")
+app.post("/blogs",async(req,res)=>{
     let {title,body} = req.body;
-    console.log(title,body);
-    res.send("GOT IT");
+    let newBlog = new Blogs({
+        title:title,
+        body:body,
+        date: Date.now()
+    })
+    await newBlog.save()
+    res.json({
+        success: true,
+        data: newBlog,
+        message: "Blog created successfully"
+    });
 })
+
+// app.post("/blogs",(req,res)=>{
+//     let {title,body} = req.body;
+//     console.log(title,body);
+//     res.send("GOT IT");
+// })
 
 app.listen(4445, function() {
     console.log('Server started on port 4445');
