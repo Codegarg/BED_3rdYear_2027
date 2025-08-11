b2a38db2f4cfd55814cf426d9a19598b6f1c8122
codegarg <ridhamgarg002@gmail.com>
2025-08-11 14:56:29 +0530

    LEC-16

--- LEC-16/index.js
+++ LEC-16/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.post("/blogs",(req,res)=>{
+    let {title,body} = req.body;
+    console.log(title,body);
+    res.send("GOT IT");
+})
 
-
-app.listen(3333, function() {
-    console.log('Server started on port 5500');
+app.listen(4445, function() {
+    console.log('Server started on port 4445');
 });
 
-mongoose.connect('mongodb://localhost:27017/LEC-16')
+mongoose.connect('mongodb://localhost:27017/BED_DB')
     .then(()=> console.log("Connected"));
