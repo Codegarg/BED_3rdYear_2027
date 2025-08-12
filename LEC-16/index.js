1d273b6135e8e9ed19196574fb3d51e42a8e2ef6
codegarg <ridhamgarg002@gmail.com>
2025-08-12 10:55:39 +0530

    LEC-16

--- LEC-16/index.js
+++ LEC-16/index.js
@@ -37,15 +37,47 @@ app.get("/blogs/:id",async(req,res)=>{
     });
 })
 
-//app.post("/users");
-//app.get("/users");
-//app.get("/users/:id");
-
 // app.post("/blogs",(req,res)=>{
 //     let {title,body} = req.body;
 //     console.log(title,body);
 //     res.send("GOT IT");
-// })
+
+//app.post("/users");
+//app.get("/users");
+//app.get("/users/:id");
+const Users = require("./model/user")
+
+app.post("/users",async(req,res)=>{
+    let {email,username,password} = req.body;
+    let newUser = new Users({
+        email:email,
+        username:username,
+        password:password
+    })
+    await newUser.save()
+    res.json({
+        success: true,
+        data: newUser,
+        message: "User created successfully"
+    });
+})
+
+app.get("/users",async(req,res)=>{
+    let allUsers = await Users.find();
+    res.json({
+        success: true,
+        data: allUsers
+    });
+})
+
+app.get("/users/:id",async(req,res)=>{
+    let {id} = req.params;
+    let user = await Users.findOne({_id: id});
+    res.json({
+        success: true,
+        data: user
+    });
+})
 
 app.listen(4445, function() {
     console.log('Server started on port 4445');
--- LEC-16/model/user.js
+++ LEC-16/model/user.js
@@ -1,3 +1,21 @@
-//email
-//username
-//password
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const user = new schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User', user)
