c2fb2a497e39f6e90cfdf38a8c87a9ca60c3c85f
codegarg <ridhamgarg002@gmail.com>
2025-08-18 15:25:38 +0530

    LEC-19

--- LEC-19/routes/userRoutes.js
+++ LEC-19/routes/userRoutes.js
@@ -1,38 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const user = require("..//model/user");
-router.post("/",async(req,res)=>{
-    let {email,username,password} = req.body;
-    let newUser = new user({
-        email:email,
-        username:username,
-        password:password
-    })
-    await newUser.save()
-    res.json({
-        success: true,
-        data: newUser,
-        message: "User created successfully"
-    });
-})
+const{ postuser, getAllUsers, getUserById } = require("../controller/userController");
 
-router.get("/",async(req,res)=>{
-    let allUsers = await user.find();
-    res.json({
-        success: true,
-        data: allUsers
-    });
-})
-
-router.get("/:id",async(req,res)=>{
-    let {id} = req.params;
-    let userExist = await user.findOne({_id: id}).populate("blogs");
-    if(userExist){
-        res.json({
-        success: true,
-        data: userExist
-    });
-    }
-})
+router.post("/",postuser)
+router.get("/",getAllUsers)
+router.get("/:id",getUserById)
 
 module.exports = router;
